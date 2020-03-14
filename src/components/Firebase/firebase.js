a29ff3cce7c30b839c8a6f81b8a56ec7e0462e6b
tarwin3v <tarwin.ev@gmail.com>
2020-03-14 16:42:37 +0100

    link socialntw account with pass/email account / 12.03

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -33,6 +33,11 @@ class Firebase {
   doSignInWithEmailAndPassword = (email, password) =>
     this.auth.signInWithEmailAndPassword(email, password);
 
+  doSendEmailVerification = () => {
+    this.auth.currentUser.sendEmailVerification({
+      url: process.env.REACT_APP_CONFIRMATION_EMAIL_REDIRECT
+    });
+  };
   //@x AUTH API - Social Networks SignIn
 
   doSignInWithGoogle = () => this.auth.signInWithPopup(this.googleProvider);
@@ -59,6 +64,8 @@ class Firebase {
             authUser = {
               uid: authUser.uid,
               email: authUser.email,
+              emailVerified: authUser.emailVerified,
+              providerData: authUser.providerData,
               ...dbUser
             };
 
